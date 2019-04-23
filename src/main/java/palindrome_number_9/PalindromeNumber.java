2b2d617310d1580345a057125a5412933f872936
Tody <tody.code@gmail.com>
2019-04-23 15:22:43 +0800

    题9解

---
+++ src/main/java/palindrome_number_9/PalindromeNumber.java
@@ -0,0 +1,33 @@
+package palindrome_number_9;
+
+/**
+ * @program: leetcode
+ * @description: 9.palindrome-number
+ * @author: hdy
+ * @create: 2019-04-23
+ **/
+
+public class PalindromeNumber {
+
+  public static boolean isPalindrome(int x) {
+    if (x < 0 || (x % 10 == 0 && x != 0)) {
+      return false;
+    }
+    StringBuffer buffer = new StringBuffer(String.valueOf(x));
+    return buffer.reverse().toString().equals(String.valueOf(x));
+  }
+
+  public static boolean anotherIsPalindrome(int x) {
+    if (x < 0 || (x % 10 == 0 && x != 0)) {
+      return false;
+    }
+    int tmp = x;
+    int rever = 0;
+    while (tmp > 0) {
+      rever = rever * 10 + tmp % 10;
+      tmp /= 10;
+    }
+
+    return rever == x;
+  }
+}
