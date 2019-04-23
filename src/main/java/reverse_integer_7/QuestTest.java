db207b12ff77026170d5eb329ed761b34820031b
Tody <tody.code@gmail.com>
2019-04-23 15:05:15 +0800

    题7解

---
+++ src/main/java/reverse_integer_7/QuestTest.java
@@ -0,0 +1,41 @@
+package reverse_integer_7;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @program: leetcode
+ * @description: main
+ * @author: hdy
+ * @create: 2019-04-23
+ **/
+
+public class QuestTest {
+
+  @Test
+  public void test0() {
+    long x = 123;
+    int result = 321;
+    Assert.assertEquals(result, ReverseInteger.anotherReverse(x));
+  }
+
+  @Test
+  public void test1() {
+    long x = -123;
+    int result = -321;
+    Assert.assertEquals(result, ReverseInteger.anotherReverse(x));
+  }
+
+  @Test
+  public void test2() {
+    long x = 120;
+    int result = 21;
+    Assert.assertEquals(result, ReverseInteger.anotherReverse(x));
+  }
+  @Test
+  public void test12() {
+    long x = 9646324351L;
+    int result = 1534236469;
+    Assert.assertEquals(result, ReverseInteger.anotherReverse(x));
+  }
+}
