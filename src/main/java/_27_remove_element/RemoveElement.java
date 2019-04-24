9952c35dff7357316887a8f3fc039ff6cf8ae81a
Tody <tody.code@gmail.com>
2019-04-24 16:40:35 +0800

    题27新解

--- src/main/java/_27_remove_element/RemoveElement.java
+++ src/main/java/_27_remove_element/RemoveElement.java
@@ -11,9 +11,9 @@ public class RemoveElement {
 
   /**
    * √ Accepted
-     √ 113/113 cases passed (0 ms)
-     √ Your runtime beats 100 % of java submissions
-     √ Your memory usage beats 95.09 % of java submissions (37.4 MB)
+   * √ 113/113 cases passed (0 ms)
+   * √ Your runtime beats 100 % of java submissions
+   * √ Your memory usage beats 95.09 % of java submissions (37.4 MB)
    */
   public static int removeElement(int[] nums, int val) {
     if (nums.length == 0) {
@@ -34,5 +34,45 @@ public class RemoveElement {
     }
     return i;
   }
+
+  /**
+   * √ Accepted
+   * √ 113/113 cases passed (0 ms)
+   * √ Your runtime beats 100 % of java submissions
+   * √ Your memory usage beats 94.58 % of java submissions (37.4 MB)
+   */
+  public static int anotherSolution(int[] nums, int val) {
+    int i = 0;
+    for (int j = 0; j < nums.length; j++) {
+      if (nums[j] != val) {
+        nums[i] = nums[j];
+        i++;
+      }
+    }
+    return i;
+  }
+
+  /**
+   * √ Accepted
+   * √ 113/113 cases passed (0 ms)
+   * √ Your runtime beats 100 % of java submissions
+   * √ Your memory usage beats 96.63 % of java submissions (37.3 MB)
+   *
+   * remove are rare
+   */
+  public static int anAnotherSolution(int[] nums, int val) {
+    int i = 0;
+    int n = nums.length;
+    while (i < n) {
+      if (nums[i] == val) {
+        nums[i] = nums[n - 1];
+        n--;
+      } else {
+        i++;
+      }
+    }
+    return i;
+  }
+
 
 }
