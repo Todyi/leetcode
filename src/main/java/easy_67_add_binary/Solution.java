04ea522efaef9fc88ef5cb666b1dc0c7818c19af
tody <tody.code@gmail.com>
2020-04-18 18:50:07 +0800

    67.Add Binary

--- src/main/java/easy_67_add_binary/Solution.java
+++ src/main/java/easy_67_add_binary/Solution.java
@@ -7,28 +7,23 @@ package easy_67_add_binary;/*
 // @lc code=start
 class Solution {
 
-  //294/294 cases passed (2 ms)
-  //Your runtime beats 78.21 % of java submissions
-  //Your memory usage beats 5.62 % of java submissions (39.3 MB)
+//294/294 cases passed (1 ms)
+//Your runtime beats 100 % of java submissions
+//Your memory usage beats 5.62 % of java submissions (38.6 MB)
   public String addBinary(String a, String b) {
-    boolean aLonger = b.length() < a.length();
-    char[] longArr = aLonger ? a.toCharArray() : b.toCharArray();
-    char[] shortArr = !aLonger ? a.toCharArray() : b.toCharArray();
+    StringBuffer sb = new StringBuffer();
+    int aFlag = a.length() - 1;
+    int bFlag = b.length() - 1;
     int carry = 0;
-    for (int i = longArr.length - 1; i >= 0; i--) {
-      int shortFlag = shortArr.length - (longArr.length - i);
-      int shortItem = 0 <= shortFlag ? Integer.valueOf(shortArr[shortFlag] - '0') : 0;
-      int longItem = Integer.valueOf(longArr[i] - '0');
-      int sum = shortItem + longItem + carry;
-      longArr[i] = (char) (sum % 2 + '0');
+    while (aFlag >= 0 || bFlag >= 0 || carry > 0){
+      int aItem = aFlag < 0 ? 0:a.charAt(aFlag--)-'0';
+      int bItem = bFlag < 0 ? 0:b.charAt(bFlag--)-'0';
+      int sum = aItem + bItem + carry;
+      sb.append(sum & 1);
       carry = sum >> 1;
     }
-    String result = new String(longArr);
-    if (carry == 1) {
-      result = "1" + result;
-    }
+    return sb.reverse().toString();
 
-    return result;
   }
 
 }
