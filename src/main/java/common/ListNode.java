bcf1fe93ca79ffc16eab857c79ab09f53a3b7340
tody <tody.code@gmail.com>
2020-04-19 14:22:37 +0800

    83.Remove duplicates form sorted list

--- src/main/java/common/ListNode.java
+++ src/main/java/common/ListNode.java
@@ -12,23 +12,7 @@ public class ListNode {
   public int val;
   public ListNode next;
 
-  ListNode(int x) {
+  public ListNode(int x) {
     val = x;
-  }
-
-  public int getVal() {
-    return val;
-  }
-
-  public void setVal(int val) {
-    this.val = val;
-  }
-
-  public ListNode getNext() {
-    return next;
-  }
-
-  public void setNext(ListNode next) {
-    this.next = next;
   }
 }
