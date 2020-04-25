a7396d4be1c7e3fb21735ca9ece8a9354475a2ec
tody <tody.code@gmail.com>
2020-04-25 22:55:14 +0800

    190 another solution

--- src/main/java/easy/_190_reverse_bits/Solution.java
+++ src/main/java/easy/_190_reverse_bits/Solution.java
@@ -60,8 +60,17 @@ public class Solution {
       n >>>= 1;
     }
     return result;
-
   }
+//    0 ms Sample
+//  public int reverseBits(int n) {
+//    int res = n;
+//    res = (res >>> 16) | (res << 16);
+//    res = (res & 0xff00ff00) >>> 8 | (res & 0x00ff00ff) << 8;
+//    res = (res & 0xf0f0f0f0) >>> 4 | (res & 0x0f0f0f0f) << 4;
+//    res = (res & 0xcccccccc) >>> 2 | (res & 0x33333333) << 2;
+//    res = (res & 0xaaaaaaaa) >>> 1 | (res & 0x55555555) << 1;
+//    return res;
+//  }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
