14ba1c8fb0b159cc8408a0bb9b039abc927d63ff
tody <tody.code@gmail.com>
2020-04-26 00:17:49 +0800

    193.Valid Phone Numbers

---
+++ src/main/java/easy/_193_valid_phone_numbers/[193]Valid Phone Numbers.sh
@@ -0,0 +1,33 @@
+#Given a text file file.txt that contains list of phone numbers (one per line),
+# write a one liner bash script to print all valid phone numbers. 
+#
+# You may assume that a valid phone number must appear in one of the following 
+#two formats: (xxx) xxx-xxxx or xxx-xxx-xxxx. (x means a digit) 
+#
+# You may also assume each line in the text file must not contain leading or tr
+#ailing white spaces. 
+#
+# Example: 
+#
+# Assume that file.txt has the following content: 
+#
+# 
+#987-123-4567
+#123 456 7890
+#(123) 456-7890
+# 
+#
+# Your script should output the following valid phone numbers: 
+#
+# 
+#987-123-4567
+#(123) 456-7890
+# 
+#
+
+
+#There is no code of Java type for this problem
+
+#Runtime: 0 ms, faster than 100.00% of Bash online submissions for Valid Phone Numbers.
+#Memory Usage: 3.1 MB, less than 96.43% of Bash online submissions for Valid Phone Numbers.
+grep -P '^(\(\d{3}\) |\d{3}-)\d{3}-\d{4}$' file.txt
