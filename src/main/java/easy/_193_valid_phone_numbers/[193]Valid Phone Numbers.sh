0871c5c5fe0415fab4e1ef317d63850819029ef4
Tody <tody.code@gmail.com>
2020-04-26 13:58:17 +0800

    193 another two solution

--- src/main/java/easy/_193_valid_phone_numbers/[193]Valid Phone Numbers.sh
+++ src/main/java/easy/_193_valid_phone_numbers/[193]Valid Phone Numbers.sh
@@ -31,3 +31,7 @@
 #Runtime: 0 ms, faster than 100.00% of Bash online submissions for Valid Phone Numbers.
 #Memory Usage: 3.1 MB, less than 96.43% of Bash online submissions for Valid Phone Numbers.
 grep -P '^(\(\d{3}\) |\d{3}-)\d{3}-\d{4}$' file.txt
+
+sed -n -r '/^([0-9]{3}-|\([0-9]{3}\) )[0-9]{3}-[0-9]{4}$/p' file.txt
+
+awk '/^([0-9]{3}-|\([0-9]{3}\) )[0-9]{3}-[0-9]{4}$/' file.txt
