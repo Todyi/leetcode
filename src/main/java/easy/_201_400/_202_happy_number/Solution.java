182947fb658157aaf8da44f9eb3653d6f945b66d
tody <tody.code@gmail.com>
2020-04-28 00:52:50 +0800

    202 another solution

--- src/main/java/easy/_201_400/_202_happy_number/Solution.java
+++ src/main/java/easy/_201_400/_202_happy_number/Solution.java
@@ -24,45 +24,72 @@ package easy._201_400._202_happy_number;
 // Related Topics Hash Table Math
 
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-  Map<Integer,Integer> existMap = new HashMap<>();
 
+  //    解答成功:
+  //    执行耗时:2 ms,击败了21.59% 的Java用户
+  //    内存消耗:36.3 MB,击败了6.06% 的Java用户
+//    Map<Integer,Integer> existMap = new HashMap<>();
+//    public boolean isHappy(int n) {
+//        List<Integer> queue = new ArrayList<>();
+//        int num = n;
+//        while (1 <= num) {
+//            Integer exist = existMap.get(num);
+//            if (exist == null){
+//                existMap.put(num,num);
+//            }else {
+//                return false;
+//            }
+//
+//            while (0 < num) {
+//                if (num % 10 != 0) {
+//                    queue.add(num % 10);
+//                }
+//                num /= 10;
+//            }
+//            if (queue.size() == 1 && queue.get(0) == 1) {
+//                return true;
+//            }
+//            if (queue.size() > 10) {
+//                break;
+//            }
+//            num = 0;
+//            for (int i = 0; i < queue.size(); i++) {
+//                num += queue.get(i) * queue.get(i);
+//            }
+//            queue.clear();
+//        }
+//        return false;
+//    }
+
+  //  解答成功:
+  //  执行耗时:1 ms,击败了84.24% 的Java用户
+  //  内存消耗:36.1 MB,击败了6.06% 的Java用户
   public boolean isHappy(int n) {
-    List<Integer> queue = new ArrayList<>();
-    int num = n;
-    while (1 <= num) {
+    Map<Integer, Integer> existMap = new HashMap<>();
+    while (1 < n) {
+      int num = n;
+      n = 0;
       Integer exist = existMap.get(num);
-      if (exist == null){
-        existMap.put(num,num);
-      }else {
+      if (exist == null) {
+        existMap.put(num, num);
+      } else {
         return false;
       }
-
       while (0 < num) {
-        if (num % 10 != 0) {
-          queue.add(num % 10);
+        int remain = num % 10;
+        if (remain != 0) {
+          n += remain * remain;
         }
         num /= 10;
       }
-      if (queue.size() == 1 && queue.get(0) == 1) {
-        return true;
-      }
-      if (queue.size() > 10) {
-        break;
-      }
-      num = 0;
-      for (int i = 0; i < queue.size(); i++) {
-        num += queue.get(i) * queue.get(i);
-      }
-      queue.clear();
     }
-    return false;
+
+    return true;
   }
 }
 //leetcode submit region end(Prohibit modification and deletion)
