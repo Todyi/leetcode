cdeda500a4e68fa4c03ba2a56685948a28a63185
Tody <tody.code@gmail.com>
2020-04-28 09:55:31 +0800

    203.Remove Linked List Elements

--- src/main/java/common/Comparators.java
+++ src/main/java/common/Comparators.java
@@ -12,38 +12,51 @@ package common;
 
 public class Comparators {
 
-  public static boolean isSame(int[] array0,int[] array1){
-    if (array0 == null && array1 == null){
+  public static boolean isSame(int[] array0, int[] array1) {
+    if (array0 == null && array1 == null) {
       return true;
     }
-    if (array0 == null || array1 == null){
+    if (array0 == null || array1 == null) {
       return false;
     }
-    if (array0.length != array1.length){
+    if (array0.length != array1.length) {
       return false;
     }
     for (int i = 0; i < array0.length; i++) {
-      if (array0[i] != array1[i]){
+      if (array0[i] != array1[i]) {
         return false;
       }
     }
     return true;
   }
 
-  public static boolean isSame(Integer[] array0,Integer[] array1){
-    return isSame(array0,array1);
+  public static boolean isSame(Integer[] array0, Integer[] array1) {
+    return isSame(array0, array1);
   }
 
 
   public static boolean isSame(TreeNode p, TreeNode q) {
-    if (p == null && q == null){
+    if (p == null && q == null) {
       return true;
     }
-    if (p == null || q == null){
+    if (p == null || q == null) {
       return false;
     }
-    if (p.val == q.val){
-      return isSame(p.left,q.left) && isSame(p.right,q.right);
+    if (p.val == q.val) {
+      return isSame(p.left, q.left) && isSame(p.right, q.right);
+    }
+    return false;
+  }
+
+  public static boolean isSame(ListNode p, ListNode q) {
+    if (p == null && q == null) {
+      return true;
+    }
+    if (p == null || q == null) {
+      return false;
+    }
+    if (p.val == q.val) {
+      return isSame(p.next, q.next);
     }
     return false;
   }
---
+++ src/main/java/easy/_201_400/_203_remove_linked_list_elements/Solution.java
@@ -0,0 +1,47 @@
+package easy._201_400._203_remove_linked_list_elements;
+
+//Remove all elements from a linked list of integers that have value val.
+//
+// Example:
+//
+//
+//Input:  1->2->6->3->4->5->6, val = 6
+//Output: 1->2->3->4->5
+//
+// Related Topics Linked List
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import common.ListNode;
+
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode next; ListNode(int
+ * x) { val = x; } }
+ */
+class Solution {
+
+  //  解答成功:
+  //  执行耗时:1 ms,击败了81.74% 的Java用户
+  //  内存消耗:40 MB,击败了100.00% 的Java用户
+  public ListNode removeElements(ListNode head, int val) {
+    if (head != null) {
+
+      while (head != null && head.val == val) {
+        head = head.next;
+      }
+      ListNode pointer = head;
+      while (pointer != null && pointer.next != null) {
+        ListNode next = pointer.next;
+        if (next != null && next.val == val) {
+          pointer.next = next.next;
+        } else {
+          pointer = pointer.next;
+        }
+      }
+      return head;
+    }
+    return null;
+  }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
