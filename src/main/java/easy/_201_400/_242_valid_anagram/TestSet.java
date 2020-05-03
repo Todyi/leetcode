689eedc8ae48ce22b0376077219902d9c5122a42
tody <tody.code@gmail.com>
2020-05-03 13:21:19 +0800

    242.Valid Anagram

---
+++ src/main/java/easy/_201_400/_242_valid_anagram/TestSet.java
@@ -0,0 +1,38 @@
+package easy._201_400._242_valid_anagram;
+
+
+import common.Comparators;
+import common.ListNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSet {
+
+  @Test
+  public void test0() {
+    String s = "anagram";
+    String t = "nagaram";
+    Boolean result = true;
+    boolean solution = new easy._201_400._242_valid_anagram.Solution().isAnagram(s, t);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test1() {
+    String s = "rat";
+    String t = "cat";
+    Boolean result = false;
+    boolean solution = new easy._201_400._242_valid_anagram.Solution().isAnagram(s, t);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test2() {
+    String s = "a";
+    String t = "ab";
+    Boolean result = false;
+    boolean solution = new easy._201_400._242_valid_anagram.Solution().isAnagram(s, t);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+}
