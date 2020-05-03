8efb8bbeaedc23666838efedb85dae73be281ee9
tody <tody.code@gmail.com>
2020-05-03 14:47:40 +0800

    257 another resolution

--- src/main/java/easy/_201_400/_257_binary_tree_paths/Resolution.java
+++ src/main/java/easy/_201_400/_257_binary_tree_paths/Resolution.java
@@ -36,24 +36,45 @@ class Solution {
   //  解答成功:
   //  执行耗时:10 ms,击败了12.42% 的Java用户
   //  内存消耗:39.6 MB,击败了5.55% 的Java用户
+//  public List<String> binaryTreePaths(TreeNode root) {
+//    List<String> result = new ArrayList<>();
+//    if (root == null) {
+//      return result;
+//    }
+//    if (root.left == null && root.right == null) {
+//      result.add(root.val + "");
+//    } else {
+//      List<String> left = binaryTreePaths(root.left);
+//      for (String s : left) {
+//        result.add(root.val + "->" + s);
+//      }
+//      List<String> right = binaryTreePaths(root.right);
+//      for (String s : right) {
+//        result.add(root.val + "->" + s);
+//      }
+//    }
+//    return result;
+//  }
+
+  //  Runtime: 1 ms, faster than 100.00% of Java online submissions for Binary Tree Paths.
+  //  Memory Usage: 39.8 MB, less than 5.55% of Java online submissions for Binary Tree Paths.
   public List<String> binaryTreePaths(TreeNode root) {
     List<String> result = new ArrayList<>();
-    if (root == null) {
-      return result;
-    }
-    if (root.left == null && root.right == null) {
-      result.add(root.val + "");
-    } else {
-      List<String> left = binaryTreePaths(root.left);
-      for (String s : left) {
-        result.add(root.val + "->" + s);
-      }
-      List<String> right = binaryTreePaths(root.right);
-      for (String s : right) {
-        result.add(root.val + "->" + s);
-      }
-    }
+    buildPaths(root, "", result);
     return result;
+  }
+
+  public void buildPaths(TreeNode node, String ancestor, List<String> result) {
+    if (node != null) {
+      ancestor += Integer.toString(node.val);
+      if (node.left == null && node.right == null) {
+        result.add(ancestor);
+      } else {
+        ancestor += "->";
+        buildPaths(node.left, ancestor, result);
+        buildPaths(node.right, ancestor, result);
+      }
+    }
   }
 
 }
