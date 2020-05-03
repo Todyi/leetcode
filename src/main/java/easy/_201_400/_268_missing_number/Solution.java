85a6bda7e040a2defe6a2db927072f753a6ed217
tody <tody.code@gmail.com>
2020-05-03 15:57:02 +0800

    268 another solution

--- src/main/java/easy/_201_400/_268_missing_number/Solution.java
+++ src/main/java/easy/_201_400/_268_missing_number/Solution.java
@@ -31,14 +31,25 @@ class Solution {
   //  解答成功:
   //  执行耗时:5 ms,击败了23.60% 的Java用户
   //  内存消耗:40 MB,击败了100.00% 的Java用户
+//  public int missingNumber(int[] nums) {
+//    Arrays.sort(nums);
+//    for (int i = 0; i < nums.length; i++) {
+//      if (nums[i] != i) {
+//        return i;
+//      }
+//    }
+//    return nums.length;
+//  }
+
+  //  Runtime: 0 ms, faster than 100.00% of Java online submissions for Missing Number.
+  //  Memory Usage: 40.3 MB, less than 98.95% of Java online submissions for Missing Number.
   public int missingNumber(int[] nums) {
-    Arrays.sort(nums);
-    for (int i = 0; i < nums.length; i++) {
-      if (nums[i] != i) {
-        return i;
-      }
+    //1+2+3+...+n = (n+1)n/2
+    int sum = nums[0];
+    for (int i = 1; i < nums.length; i++) {
+      sum += nums[i];
     }
-    return nums.length;
+    return (((nums.length + 1) * nums.length) >> 1) - sum;
   }
 }
 //leetcode submit region end(Prohibit modification and deletion)
