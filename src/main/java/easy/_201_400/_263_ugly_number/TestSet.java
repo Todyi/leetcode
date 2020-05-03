1715b095c7079857a285be133f9759b347ecdf01
tody <tody.code@gmail.com>
2020-05-03 15:11:04 +0800

    263.Ugly Number

---
+++ src/main/java/easy/_201_400/_263_ugly_number/TestSet.java
@@ -0,0 +1,43 @@
+package easy._201_400._263_ugly_number;
+
+
+import common.Comparators;
+import common.ListNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSet {
+
+  @Test
+  public void test0() {
+    int input = 6;
+    Boolean result = true;
+    boolean resolution = new easy._201_400._263_ugly_number.Solution().isUgly(input);
+    Assert.assertTrue(result.equals(resolution));
+  }
+
+  @Test
+  public void test1() {
+    int input = 8;
+    Boolean result = true;
+    boolean resolution = new easy._201_400._263_ugly_number.Solution().isUgly(input);
+    Assert.assertTrue(result.equals(resolution));
+  }
+
+  @Test
+  public void test2() {
+    int input = 14;
+    Boolean result = false;
+    boolean resolution = new easy._201_400._263_ugly_number.Solution().isUgly(input);
+    Assert.assertTrue(result.equals(resolution));
+  }
+
+  @Test
+  public void test3() {
+    int input = -2147483648;
+    Boolean result = false;
+    boolean resolution = new easy._201_400._263_ugly_number.Solution().isUgly(input);
+    Assert.assertTrue(result.equals(resolution));
+  }
+
+}
