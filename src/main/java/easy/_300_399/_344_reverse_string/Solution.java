ea4d7b4c4008267090ecdf17e477404e95f12977
tody <tody.code@gmail.com>
2020-05-04 13:28:54 +0800

    344.Reverse String

---
+++ src/main/java/easy/_300_399/_344_reverse_string/Solution.java
@@ -0,0 +1,51 @@
+package easy._300_399._344_reverse_string;
+
+//Write a function that reverses a string. The input string is given as an array
+// of characters char[].
+//
+// Do not allocate extra space for another array, you must do this by modifying
+//the input array in-place with O(1) extra memory.
+//
+// You may assume all the characters consist of printable ascii characters.
+//
+//
+//
+//
+// Example 1:
+//
+//
+//Input: ["h","e","l","l","o"]
+//Output: ["o","l","l","e","h"]
+//
+//
+//
+// Example 2:
+//
+//
+//Input: ["H","a","n","n","a","h"]
+//Output: ["h","a","n","n","a","H"]
+//
+//
+//
+// Related Topics Two Pointers String
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+  //  解答成功:
+  //  执行耗时:1 ms,击败了72.48% 的Java用户
+  //  内存消耗:46.6 MB,击败了94.67% 的Java用户
+  public void reverseString(char[] s) {
+    if (s != null && 1 < s.length) {
+      int l = s.length - 1;
+      for (int i = 0; i < s.length / 2; i++) {
+        s[i] = (char) (s[i] ^ s[l - i]);
+        s[l - i] = (char) (s[i] ^ s[l - i]);
+        s[i] = (char) (s[i] ^ s[l - i]);
+      }
+    }
+  }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
---
+++ src/main/java/easy/_300_399/_344_reverse_string/TestSet.java
@@ -0,0 +1,26 @@
+package easy._300_399._344_reverse_string;
+
+
+import common.Comparators;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSet {
+
+  @Test
+  public void test0() {
+    char[] input = {'h','e','l','l','o'};
+    char[] result = {'o','l','l','e','h'};
+    new easy._300_399._344_reverse_string.Solution().reverseString(input);
+    Assert.assertTrue(Comparators.isSame(result,input));
+  }
+  
+  @Test
+  public void test1() {
+    char[] input = {'H','a','n','n','a','h'};
+    char[] result = {'h','a','n','n','a','H'};
+    new easy._300_399._344_reverse_string.Solution().reverseString(input);
+    Assert.assertTrue(Comparators.isSame(result,input));
+  }
+
+}
