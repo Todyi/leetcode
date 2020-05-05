4337d0a4e5b957cc319b692ca7e1c0949d976d49
tody <tody.code@gmail.com>
2020-05-05 14:05:19 +0800

    387 another solution

--- src/main/java/easy/_300_399/_387_first_unique_character_in_a_string/Solution.java
+++ src/main/java/easy/_300_399/_387_first_unique_character_in_a_string/Solution.java
@@ -24,24 +24,38 @@ class Solution {
   //  解答成功:
   //  执行耗时:4 ms,击败了98.36% 的Java用户
   //  内存消耗:39.4 MB,击败了51.43% 的Java用户
+//  public int firstUniqChar(String s) {
+//    if (s.length() == 1) {
+//      return 0;
+//    }
+//    char[] arr = s.toCharArray();
+//
+//    int[] map = new int[128];
+//    for (int i = 0; i < arr.length; i++) {
+//      map[arr[i]]++;
+//    }
+//    int p = Integer.MAX_VALUE;
+//    for (int i = 0; i < map.length; i++) {
+//      if (1 == map[i]) {
+//        p = Math.min(p, s.indexOf((char) i));
+//      }
+//
+//    }
+//
+//    return p == Integer.MAX_VALUE ? -1 : p;
+//  }
+
+  //  解答成功:
+  //  执行耗时:1 ms,击败了100.00% 的Java用户
+  //  内存消耗:40.3 MB,击败了5.71% 的Java用户
   public int firstUniqChar(String s) {
-    if (s.length() == 1) {
-      return 0;
-    }
-    char[] arr = s.toCharArray();
-
-    int[] map = new int[128];
-    for (int i = 0; i < arr.length; i++) {
-      map[arr[i]]++;
-    }
     int p = Integer.MAX_VALUE;
-    for (int i = 0; i < map.length; i++) {
-      if (1 == map[i]) {
-        p = Math.min(p, s.indexOf((char) i));
+    for (int i = 'a'; i <= 'z'; i++) {
+      int firstP = s.indexOf(i);
+      if (-1 < firstP && firstP == s.lastIndexOf(i)) {
+        p = Math.min(p, firstP);
       }
-
     }
-
     return p == Integer.MAX_VALUE ? -1 : p;
   }
 }
