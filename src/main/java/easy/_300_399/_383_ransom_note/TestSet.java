f8240f101408ea4ee1e87c28f2403ad151803072
tody <tody.code@gmail.com>
2020-05-05 12:59:23 +0800

    383.Ransom Node

---
+++ src/main/java/easy/_300_399/_383_ransom_note/TestSet.java
@@ -0,0 +1,38 @@
+package easy._300_399._383_ransom_note;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSet {
+
+  @Test
+  public void test0() {
+    String ransomNote = "a";
+    String magazine = "b";
+    Boolean result = false;
+    boolean solution = new easy._300_399._383_ransom_note.Solution()
+        .canConstruct(ransomNote, magazine);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test1() {
+    String ransomNote = "aa";
+    String magazine = "ab";
+    Boolean result = false;
+    boolean solution = new easy._300_399._383_ransom_note.Solution()
+        .canConstruct(ransomNote, magazine);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test2() {
+    String ransomNote = "aa";
+    String magazine = "aab";
+    Boolean result = true;
+    boolean solution = new easy._300_399._383_ransom_note.Solution()
+        .canConstruct(ransomNote, magazine);
+    Assert.assertTrue(result.equals(solution));
+  }
+}
