d0f48062a0154bb7c3d81fc126f9f0d923a7d7f3
Tody <tody.code@gmail.com>
2020-05-06 16:26:34 +0800

    404.Sum of Left Leaves

---
+++ src/main/java/easy/_400_499/_404_sum_of_left_leaves/TestSet.java
@@ -0,0 +1,34 @@
+package easy._400_499._404_sum_of_left_leaves;
+
+
+import common.TreeNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSet {
+
+  @Test
+  public void test0() {
+    TreeNode root = TreeNode.buildTree(new Integer[]{3, 9, 20, null, null, 15, 7});
+    Integer result = 24;
+    int solution = new easy._400_499._404_sum_of_left_leaves.Solution().sumOfLeftLeaves(root);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test1() {
+    TreeNode root = TreeNode.buildTree(new Integer[]{1});
+    Integer result = 0;
+    int solution = new easy._400_499._404_sum_of_left_leaves.Solution().sumOfLeftLeaves(root);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test2() {
+    TreeNode root = TreeNode.buildTree(new Integer[]{1,null,2});
+    Integer result = 0;
+    int solution = new easy._400_499._404_sum_of_left_leaves.Solution().sumOfLeftLeaves(root);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+}
