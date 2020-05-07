5ce6b72502c93a1fe82f62d5749f205dd420a605
Tody <tody.code@gmail.com>
2020-05-07 17:29:07 +0800

    415.Add Strings

---
+++ src/main/java/easy/_400_499/_415_add_strings/Solution.java
@@ -0,0 +1,45 @@
+package easy._400_499._415_add_strings;
+
+//Given two non-negative integers num1 and num2 represented as string, return the sum of num1 and num2.
+//
+// Note:
+//
+// The length of both num1 and num2 is < 5100.
+// Both num1 and num2 contains only digits 0-9.
+// Both num1 and num2 does not contain any leading zero.
+// You must not use any built-in BigInteger library or convert the inputs to int
+//eger directly.
+//
+// Related Topics String
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+  //  解答成功:
+  //  执行耗时:2 ms,击败了96.69% 的Java用户
+  //  内存消耗:39.4 MB,击败了6.25% 的Java用户
+  public String addStrings(String num1, String num2) {
+    if (num1.length() < num2.length()) {
+      return addStrings(num2, num1);
+    }
+    StringBuilder sb = new StringBuilder();
+    int n1L = num1.length();
+    int n2L = num2.length();
+    int carry = 0;
+    for (int i = 1; i <= n1L; i++) {
+      int n1 = num1.charAt(n1L - i) - '0';
+      int n2 = -1 < n2L - i ? num2.charAt(n2L - i) - '0' : 0;
+      int sum = (n1 + n2 + carry) % 10;
+      carry = (n1 + n2 + carry) / 10;
+      sb.append(sum);
+    }
+    if (0 < carry) {
+      sb.append(carry);
+    }
+
+    return sb.reverse().toString();
+  }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
