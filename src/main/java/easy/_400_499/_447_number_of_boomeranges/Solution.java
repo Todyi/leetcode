3f34a03ed3c8da9362e97728d3f3c2e123dad60e
Tody <tody.code@gmail.com>
2020-05-08 17:25:01 +0800

    447.Number of Boomeranges

---
+++ src/main/java/easy/_400_499/_447_number_of_boomeranges/Solution.java
@@ -0,0 +1,61 @@
+package easy._400_499._447_number_of_boomeranges;
+
+//Given n points in the plane that are all pairwise distinct, a "boomerang" is
+// a tuple of points (i, j, k) such that the distance between i and j equals the
+// distance between i and k (the order of the tuple matters).
+//
+// Find the number of boomerangs. You may assume that n will be at most 500 and
+//coordinates of points are all in the range [-10000, 10000] (inclusive).
+//
+// Example:
+//
+//
+//Input:
+//[[0,0],[1,0],[2,0]]
+//
+//Output:
+//2
+//
+//Explanation:
+//The two boomerangs are [[1,0],[0,0],[2,0]] and [[1,0],[2,0],[0,0]]
+//
+//
+//
+// Related Topics Hash Table
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+  //  解答成功:
+  //  执行耗时:146 ms,击败了36.88% 的Java用户
+  //  内存消耗:39.3 MB,击败了71.43% 的Java用户
+  public int numberOfBoomerangs(int[][] points) {
+    int count = 0;
+    for (int i = 0; i < points.length; i++) {
+      Map<Double, Integer> map = new HashMap<>();
+      for (int j = 0; j < points.length; j++) {
+        if (i == j) {
+          continue;
+        }
+        double distance = distance(points[i], points[j]);
+        if (map.containsKey(distance)) {
+          count += map.get(distance) * 2;
+        }
+        map.put(distance, map.getOrDefault(distance, 0) + 1);
+      }
+    }
+    return count;
+  }
+
+  public double distance(int[] p1, int[] p2) {
+    return Math.pow(p1[0] - p2[0], 2) + Math.pow(p1[1] - p2[1], 2);
+  }
+
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
---
+++ src/main/java/easy/_400_499/_447_number_of_boomeranges/TestSet.java
@@ -0,0 +1,28 @@
+package easy._400_499._447_number_of_boomeranges;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSet {
+
+
+  @Test
+  public void test0() {
+    int[][] points = {{0, 0}, {1, 0}, {2, 0}};
+    Integer result = 2;
+    int solution = new easy._400_499._447_number_of_boomeranges.Solution()
+        .numberOfBoomerangs(points);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test1() {
+    int[][] points = {{0, 0}, {1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    Integer result = 20;
+    int solution = new easy._400_499._447_number_of_boomeranges.Solution()
+        .numberOfBoomerangs(points);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+}
