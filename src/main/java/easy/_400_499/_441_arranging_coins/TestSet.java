fed1aea0d4db7902225e5d586ac22ad9822079f5
Tody <tody.code@gmail.com>
2020-05-08 14:54:39 +0800

    441.Arranging Coins

---
+++ src/main/java/easy/_400_499/_441_arranging_coins/TestSet.java
@@ -0,0 +1,36 @@
+package easy._400_499._441_arranging_coins;
+
+
+import common.TreeNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSet {
+
+
+  @Test
+  public void test0() {
+    int n = 5;
+    Integer result = 2;
+    int solution = new easy._400_499._441_arranging_coins.Solution().arrangeCoins(n);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+
+  @Test
+  public void test1() {
+    int n = 8;
+    Integer result = 3;
+    int solution = new easy._400_499._441_arranging_coins.Solution().arrangeCoins(n);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test2() {
+    int n = 1804289383;
+    Integer result = 60070;
+    int solution = new easy._400_499._441_arranging_coins.Solution().arrangeCoins(n);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+}
