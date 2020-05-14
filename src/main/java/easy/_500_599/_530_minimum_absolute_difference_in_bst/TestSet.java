d5a6db98c6f61c434f7a7af5942a8f8d4bb0f07e
Tody <tody.code@gmail.com>
2020-05-14 10:42:44 +0800

    530.Minimum Absolute Difference in BST

---
+++ src/main/java/easy/_500_599/_530_minimum_absolute_difference_in_bst/TestSet.java
@@ -0,0 +1,19 @@
+package easy._500_599._530_minimum_absolute_difference_in_bst;
+
+
+import common.TreeNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSet {
+
+  @Test
+  public void test0() {
+    TreeNode root = TreeNode.buildTree(new Integer[]{1, null, 3, 2});
+    Integer result = 1;
+    int solution = new easy._500_599._530_minimum_absolute_difference_in_bst.Solution()
+        .getMinimumDifference(root);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+}
