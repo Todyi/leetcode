47bba81d4d78bb5019eb69a4c2cb7cdbdd244db9
Tody <tody.code@gmail.com>
2020-05-16 14:04:22 +0800

    575.Distribute Candies

---
+++ src/main/java/easy/_500_599/_575_distribute_candies/TestSet.java
@@ -0,0 +1,36 @@
+package easy._500_599._575_distribute_candies;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSet {
+
+  @Test
+  public void test0() {
+    int[] candies = new int[]{1,1,2,2,3,3};
+    Integer result = 3;
+    int solution = new easy._500_599._575_distribute_candies.Solution()
+        .distributeCandies(candies);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test1() {
+    int[] candies = new int[]{1,1,2,3};
+    Integer result = 2;
+    int solution = new easy._500_599._575_distribute_candies.Solution()
+        .distributeCandies(candies);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test2() {
+    int[] candies = new int[]{1,1,1,1,2,2,2,3,3,3};
+    Integer result = 3;
+    int solution = new easy._500_599._575_distribute_candies.Solution()
+        .distributeCandies(candies);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+}
