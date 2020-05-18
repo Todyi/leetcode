159add27adda254cc0485bcf925383dd3a5d5475
Tody <tody.code@gmail.com>
2020-05-18 14:49:43 +0800

    594 another solution

--- src/main/java/easy/_500_599/_594_longest_harmonious_subsequence/Solution.java
+++ src/main/java/easy/_500_599/_594_longest_harmonious_subsequence/Solution.java
@@ -21,6 +21,7 @@ package easy._500_599._594_longest_harmonious_subsequence;
 // Related Topics Hash Table
 
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,16 +31,37 @@ class Solution {
   //  解答成功:
   //  执行耗时:19 ms,击败了45.15% 的Java用户
   //  内存消耗:41 MB,击败了84.21% 的Java用户
+//  public int findLHS(int[] nums) {
+//    Map<Integer, Integer> map = new HashMap<>();
+//    int len = 0;
+//    for (int num : nums) {
+//      map.put(num, map.getOrDefault(num, 0) + 1);
+//    }
+//    for (Integer key : map.keySet()) {
+//      int cur = map.get(key);
+//      int max = Math.max(map.getOrDefault(key - 1, -cur), map.getOrDefault(key + 1, -cur));
+//      len = Math.max(len, cur + max);
+//    }
+//    return len;
+//  }
+
+  //  解答成功:
+  //  执行耗时:10 ms,击败了100.00% 的Java用户
+  //  内存消耗:40.1 MB,击败了94.74% 的Java用户
   public int findLHS(int[] nums) {
-    Map<Integer, Integer> map = new HashMap<>();
-    int len = 0;
-    for (int num : nums) {
-      map.put(num, map.getOrDefault(num, 0) + 1);
-    }
-    for (Integer key : map.keySet()) {
-      int cur = map.get(key);
-      int max = Math.max(map.getOrDefault(key - 1, -cur), map.getOrDefault(key + 1, -cur));
-      len = Math.max(len, cur + max);
+    Arrays.sort(nums);
+    int len = 0, subOne = 0;
+    for (int i = 1; i < nums.length; ) {
+      if (nums[i] == nums[subOne]) {
+        i++;
+      } else if (1 == nums[i] - nums[subOne]) {
+        //1,1,2,2,3,3,3,4
+        len = Math.max(len, i - subOne + 1);
+        i++;
+      } else {
+        subOne++;
+      }
+
     }
     return len;
   }
