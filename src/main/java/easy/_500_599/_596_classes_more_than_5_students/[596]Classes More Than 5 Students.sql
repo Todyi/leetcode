ec7b6eca3bb37a430d2eb04e3964273afdec30cb
Tody <tody.code@gmail.com>
2020-05-18 15:26:12 +0800

    596.Classes More Than 5 Students

---
+++ src/main/java/easy/_500_599/_596_classes_more_than_5_students/[596]Classes More Than 5 Students.sql
@@ -0,0 +1,44 @@
+-- There is a table courses with columns: student and class 
+-- 
+--  Please list out all classes which have more than or equal to 5 students. 
+-- 
+--  For example, the table: 
+-- 
+--  
+-- +---------+------------+
+-- | student | class      |
+-- +---------+------------+
+-- | A       | Math       |
+-- | B       | English    |
+-- | C       | Math       |
+-- | D       | Biology    |
+-- | E       | Math       |
+-- | F       | Computer   |
+-- | G       | Math       |
+-- | H       | Math       |
+-- | I       | Math       |
+-- +---------+------------+
+--  
+-- 
+--  Should output: 
+-- 
+--  
+-- +---------+
+-- | class   |
+-- +---------+
+-- | Math    |
+-- +---------+
+--  
+-- 
+--  
+-- 
+--  Note: 
+-- The students should not be counted duplicate in each course. 
+-- 
+
+
+-- There is no code of Java type for this problem
+
+-- Runtime: 272 ms, faster than 67.00% of MySQL online submissions for Classes More Than 5 Students.
+-- Memory Usage: 0B, less than 100.00% of MySQL online submissions for Classes More Than 5 Students.
+SELECT class FROM courses GROUP BY class HAVING COUNT(DISTINCT student)>4
