a4beef5a31bb70cc8c88de634a6d98664e737e3f
Tody <tody.code@gmail.com>
2020-05-19 23:45:10 +0800

    605.Can Place Flowers

---
+++ src/main/java/easy/_600_699/_605_can_place_flowers/TestSet.java
@@ -0,0 +1,68 @@
+package easy._600_699._605_can_place_flowers;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSet {
+
+  @Test
+  public void test0() {
+    int[] flowerbed = {1,0,0,0,1};
+    int n = 1;
+    Boolean result = true;
+    boolean solution = new easy._600_699._605_can_place_flowers.Solution()
+        .canPlaceFlowers(flowerbed, n);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test1() {
+    int[] flowerbed = {1,0,0,0,1};
+    int n = 2;
+    Boolean result = false;
+    boolean solution = new easy._600_699._605_can_place_flowers.Solution()
+        .canPlaceFlowers(flowerbed, n);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test2() {
+    int[] flowerbed = {0,0,1,0,1};
+    int n = 1;
+    Boolean result = true;
+    boolean solution = new easy._600_699._605_can_place_flowers.Solution()
+        .canPlaceFlowers(flowerbed, n);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test3() {
+    int[] flowerbed = {0};
+    int n = 1;
+    Boolean result = true;
+    boolean solution = new easy._600_699._605_can_place_flowers.Solution()
+        .canPlaceFlowers(flowerbed, n);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test4() {
+    int[] flowerbed = {1,0,0,0,1,0,0};
+    int n = 2;
+    Boolean result = true;
+    boolean solution = new easy._600_699._605_can_place_flowers.Solution()
+        .canPlaceFlowers(flowerbed, n);
+    Assert.assertTrue(result.equals(solution));
+  }
+  @Test
+  public void test5() {
+    int[] flowerbed = {0,0,1,0,0};
+    int n = 1;
+    Boolean result = true;
+    boolean solution = new easy._600_699._605_can_place_flowers.Solution()
+        .canPlaceFlowers(flowerbed, n);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+}
