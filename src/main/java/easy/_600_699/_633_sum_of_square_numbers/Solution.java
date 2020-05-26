f2e85566c181d824d414aff9192cfdadf9a94ed5
Tody <tody.code@gmail.com>
2020-05-26 15:55:52 +0800

    633 explain

--- src/main/java/easy/_600_699/_633_sum_of_square_numbers/Solution.java
+++ src/main/java/easy/_600_699/_633_sum_of_square_numbers/Solution.java
@@ -49,6 +49,10 @@ class Solution {
   //  执行耗时:0 ms,击败了100.00% 的Java用户
   //  内存消耗:36.5 MB,击败了7.14% 的Java用户
   public boolean judgeSquareSum(int c) {
+    //Fermat's Theorem:
+    // Any positive number n is expressible as a sum of two squares
+    // if and only if the prime factorization of n, 
+    // every prime of the form (4k+3) occurs an even number of times.
     for (int i = 2; i * i <= c; i++) {
       int count = 0;
       if (c % i == 0) {
