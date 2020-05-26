1d8b4eaae9e2e30e22194354ef1c156c22461e91
Tody <tody.code@gmail.com>
2020-05-26 17:01:31 +0800

    633 explain

--- src/main/java/easy/_600_699/_633_sum_of_square_numbers/Solution.java
+++ src/main/java/easy/_600_699/_633_sum_of_square_numbers/Solution.java
@@ -49,10 +49,16 @@ class Solution {
   //  执行耗时:0 ms,击败了100.00% 的Java用户
   //  内存消耗:36.5 MB,击败了7.14% 的Java用户
   public boolean judgeSquareSum(int c) {
-    //Fermat's Theorem:
+    // Fermat's Theorem:
+    // Sum of two squares states that an odd prime p can be expressed as:
+    // p = x^2+y^2,with x and y integers,
+    // if and only if p ≡1(mod 4)
+
+    // base on Fermat's Theorem:
     // Any positive number n is expressible as a sum of two squares
     // if and only if the prime factorization of n, 
     // every prime of the form (4k+3) occurs an even number of times.
+    // proof:http://math.uga.edu/~pete/4400twosquares.pdf
     for (int i = 2; i * i <= c; i++) {
       int count = 0;
       if (c % i == 0) {
