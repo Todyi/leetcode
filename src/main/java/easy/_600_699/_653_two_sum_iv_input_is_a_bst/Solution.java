068a3d65a4ab203318f91ff45224a6c133627f2b
Tody <tody.code@gmail.com>
2020-05-29 13:52:02 +0800

    653 another solution

--- src/main/java/easy/_600_699/_653_two_sum_iv_input_is_a_bst/Solution.java
+++ src/main/java/easy/_600_699/_653_two_sum_iv_input_is_a_bst/Solution.java
@@ -55,21 +55,46 @@ class Solution {
   //  解答成功:
   //  执行耗时:2 ms,击败了94.72% 的Java用户
   //  内存消耗:41.1 MB,击败了76.79% 的Java用户
+//  public boolean findTarget(TreeNode root, int k) {
+//    Map<Integer, Integer> map = new HashMap<>();
+//    return preorder(root, map, k);
+//  }
+//
+//  public boolean preorder(TreeNode node, Map<Integer, Integer> map, int k) {
+//    if (node == null) {
+//      return false;
+//    }
+//    if (map.get(k - node.val) != null) {
+//      return true;
+//    }
+//    map.put(node.val, 1);
+//
+//    return preorder(node.left, map, k) || preorder(node.right, map, k);
+//  }
+
+  //  解答成功:
+  //  执行耗时:1 ms,击败了100.00% 的Java用户
+  //  内存消耗:40.3 MB,击败了78.57% 的Java用户
   public boolean findTarget(TreeNode root, int k) {
-    Map<Integer, Integer> map = new HashMap<>();
-    return preorder(root, map, k);
+    return dfs(root, root, k);
   }
 
-  public boolean preorder(TreeNode node, Map<Integer, Integer> map, int k) {
-    if (node == null) {
+  public boolean dfs(TreeNode pointer, TreeNode tree, int val) {
+    if (pointer == null) {
       return false;
     }
-    if (map.get(k - node.val) != null) {
-      return true;
-    }
-    map.put(node.val, 1);
+    return search(pointer, tree, val - pointer.val)
+        || dfs(pointer.left, tree, val)
+        || dfs(pointer.right, tree, val);
+  }
 
-    return preorder(node.left, map, k) || preorder(node.right, map, k);
+  public boolean search(TreeNode pointer, TreeNode tree, int val) {
+    if (tree == null) {
+      return false;
+    }
+    return (val == tree.val && pointer != tree)
+        || (val < tree.val && search(pointer, tree.left, val))
+        || (tree.val < val && search(pointer, tree.right, val));
   }
 }
 //leetcode submit region end(Prohibit modification and deletion)
