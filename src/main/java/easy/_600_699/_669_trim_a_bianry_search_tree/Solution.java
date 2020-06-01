d715ad67b26e643a5370212e57f403f35b08ff02
Tody <tody.code@gmail.com>
2020-06-01 15:21:26 +0800

    669.Trim a Binary Search Tree

---
+++ src/main/java/easy/_600_699/_669_trim_a_bianry_search_tree/Solution.java
@@ -0,0 +1,104 @@
+package easy._600_699._669_trim_a_bianry_search_tree;
+
+//
+//Given a binary search tree and the lowest and highest boundaries as L and R,
+// trim the tree so that all its elements lies in [L, R] (R >= L). You might need to
+// change the root of the tree, so the result should return the new root of the trimmed binary search tree.
+//
+//
+// Example 1:
+//
+//Input:
+//    1
+//   / \
+//  0   2
+//
+//  L = 1
+//  R = 2
+//
+//Output:
+//    1
+//      \
+//       2
+//
+//
+//
+// Example 2:
+//
+//Input:
+//    3
+//   / \
+//  0   4
+//   \
+//    2
+//   /
+//  1
+//
+//  L = 1
+//  R = 3
+//
+//Output:
+//      3
+//     /
+//   2
+//  /
+// 1
+//
+// Related Topics Tree
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import common.TreeNode;
+
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode left; TreeNode
+ * right; TreeNode() {} TreeNode(int val) { this.val = val; } TreeNode(int val, TreeNode left,
+ * TreeNode right) { this.val = val; this.left = left; this.right = right; } }
+ */
+class Solution {
+
+  //  解答成功:
+  //  执行耗时:0 ms,击败了100.00% 的Java用户
+  //  内存消耗:39.3 MB,击败了5.55% 的Java用户
+  public TreeNode trimBST(TreeNode root, int L, int R) {
+    if (root == null) {
+      return null;
+    }
+    while (true) {
+      if (root.val < L) {
+        root = root.right;
+      } else if (R < root.val) {
+        root = root.left;
+      } else {
+        break;
+      }
+    }
+    trim(root, root.left, L, true);
+    trim(root, root.right, R, false);
+    return root;
+  }
+
+  public void trim(TreeNode node, TreeNode tree, int limit, boolean less) {
+    if (tree == null) {
+      return;
+    }
+    if (less) {
+      if (tree.val < limit) {
+        node.left = tree.right;
+        trim(node, node.left, limit, true);
+      } else {
+        trim(tree, tree.left, limit, true);
+      }
+    } else {
+      if (limit < tree.val) {
+        node.right = tree.left;
+        trim(node, node.right, limit, false);
+      } else {
+        trim(tree, tree.right, limit, false);
+      }
+    }
+  }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
---
+++ src/main/java/easy/_600_699/_669_trim_a_bianry_search_tree/TestSet.java
@@ -0,0 +1,44 @@
+package easy._600_699._669_trim_a_bianry_search_tree;
+
+
+import common.Comparators;
+import common.TreeNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSet {
+
+  @Test
+  public void test0() {
+    TreeNode root = TreeNode.buildTree(new Integer[]{1, 0, 2});
+    int L = 1;
+    int R = 2;
+    TreeNode result = TreeNode.buildTree(new Integer[]{1, null, 2});
+    TreeNode solution = new easy._600_699._669_trim_a_bianry_search_tree.Solution()
+        .trimBST(root, L, R);
+    Assert.assertTrue(Comparators.isSame(result, solution));
+  }
+
+  @Test
+  public void test1() {
+    TreeNode root = TreeNode.buildTree(new Integer[]{3, 0, 4, null, 2, null, null, 1});
+    int L = 1;
+    int R = 3;
+    TreeNode result = TreeNode.buildTree(new Integer[]{3, 2, null, 1});
+    TreeNode solution = new easy._600_699._669_trim_a_bianry_search_tree.Solution()
+        .trimBST(root, L, R);
+    Assert.assertTrue(Comparators.isSame(result, solution));
+  }
+
+  @Test
+  public void test2() {
+    TreeNode root = TreeNode.buildTree(new Integer[]{4,2,5,1,3});
+    int L = 3;
+    int R = 5;
+    TreeNode result = TreeNode.buildTree(new Integer[]{4,3,5});
+    TreeNode solution = new easy._600_699._669_trim_a_bianry_search_tree.Solution()
+        .trimBST(root, L, R);
+    Assert.assertTrue(Comparators.isSame(result, solution));
+  }
+
+}
