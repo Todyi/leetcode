4c9ff3e6994da0c08daedbff345b45e51275b02b
Tody <tody.code@gmail.com>
2020-06-02 11:45:20 +0800

    682.Baseball Game

---
+++ src/main/java/easy/_600_699/_682_baseball_game/TestSet.java
@@ -0,0 +1,27 @@
+package easy._600_699._682_baseball_game;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSet {
+
+  @Test
+  public void test0() {
+    String[] ops = {"5","2","C","D","+"};
+    Integer result = 30;
+    int solution = new easy._600_699._682_baseball_game.Solution()
+        .calPoints(ops);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test1() {
+    String[] ops = {"5","-2","4","C","D","9","+","+"};
+    Integer result = 27;
+    int solution = new easy._600_699._682_baseball_game.Solution()
+        .calPoints(ops);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+}
