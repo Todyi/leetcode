58c3c1506741ec9b5d86aba3a5be1f8d8ae8c263
Tody <tody.code@gmail.com>
2020-06-05 16:30:41 +0800

    696.Count Binary Substrings

---
+++ src/main/java/easy/_600_699/_696_count_binary_substrings/TestSet.java
@@ -0,0 +1,36 @@
+package easy._600_699._696_count_binary_substrings;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSet {
+
+  @Test
+  public void test0() {
+    String s = "00110011";
+    Integer result = 6;
+    int solution = new easy._600_699._696_count_binary_substrings.Solution()
+        .countBinarySubstrings(s);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test1() {
+    String s = "10101";
+    Integer result = 4;
+    int solution = new easy._600_699._696_count_binary_substrings.Solution()
+        .countBinarySubstrings(s);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test2() {
+    String s = "0001110011";
+    Integer result = 7;
+    int solution = new easy._600_699._696_count_binary_substrings.Solution()
+        .countBinarySubstrings(s);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+}
