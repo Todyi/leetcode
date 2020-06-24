f1db35feedfcf73fa71e6b23d584b098af0ad28a
Tody <tody.code@gmail.com>
2020-06-24 14:01:22 +0800

    697 another solution

--- src/main/java/easy/_600_699/_697_degree_of_an_array/Solution.java
+++ src/main/java/easy/_600_699/_697_degree_of_an_array/Solution.java
@@ -40,37 +40,65 @@ class Solution {
   //  解答成功:
   //  执行耗时:14 ms,击败了81.99% 的Java用户
   //  内存消耗:44.7 MB,击败了25.03% 的Java用户
-  public int findShortestSubArray(int[] nums) {
-    Map<Integer, Integer[]> map = new HashMap<>();
-    int degree = Integer.MIN_VALUE, len = Integer.MAX_VALUE;
+//  public int findShortestSubArray(int[] nums) {
+//    Map<Integer, Integer[]> map = new HashMap<>();
+//    int degree = Integer.MIN_VALUE, len = Integer.MAX_VALUE;
+//
+//    for (int i = 0; i < nums.length; i++) {
+//      Integer[] num;
+//      if (map.containsKey(nums[i])) {
+//        num = map.get(nums[i]);
+//      } else {
+//        num = new Integer[3];
+//        num[0] = 0;
+//        num[1] = -1;
+//        num[2] = -1;
+//        map.put(nums[i], num);
+//      }
+//      num[0]++;
+//      if (num[1] == -1) {
+//        num[1] = i;
+//        num[2] = i;
+//      } else {
+//        num[2] = i;
+//      }
+//      if (degree < num[0]) {
+//        degree = num[0];
+//        len = num[2] - num[1] + 1;
+//      } else if (num[0] == degree && num[2] != -1 && num[2] - num[1] + 1 < len) {
+//        len = num[2] - num[1] + 1;
+//      }
+//    }
+//    return len;
+//  }
 
-    for (int i = 0; i < nums.length; i++) {
-      Integer[] num;
-      if (map.containsKey(nums[i])) {
-        num = map.get(nums[i]);
-      } else {
-        num = new Integer[3];
-        num[0] = 0;
-        num[1] = -1;
-        num[2] = -1;
-        map.put(nums[i], num);
-      }
-      num[0]++;
-      if (num[1] == -1) {
-        num[1] = i;
-        num[2] = i;
-      } else {
-        num[2] = i;
-      }
-      if (degree < num[0]) {
-        degree = num[0];
-        len = num[2] - num[1] + 1;
-      } else if (num[0] == degree && num[2] != -1 && num[2] - num[1] + 1 < len) {
-        len = num[2] - num[1] + 1;
+  //  解答成功:
+  //  执行耗时:2 ms,击败了100.00% 的Java用户
+  //  内存消耗:42.5 MB,击败了93.41% 的Java用户
+  public int findShortestSubArray(int[] nums) {
+    int max = Integer.MIN_VALUE;
+    for (int i : nums) {
+      if (max < i) {
+        max = i;
       }
     }
-    return len;
+    int[] count = new int[max + 1], start = new int[max + 1];
+    int degree = 0, res = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (count[nums[i]] == 0) {
+        start[nums[i]] = i;
+      }
+      count[nums[i]]++;
+      if (degree < count[nums[i]]) {
+        degree = count[nums[i]];
+        res = i - start[nums[i]] + 1;
+      } else if (degree == count[nums[i]]) {
+        res = Math.min(res, i - start[nums[i]] + 1);
+      }
+    }
+    return res;
   }
+
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
