c5464346ecee079469362bd2c7e52f71406f37b5
Tody <tody.code@gmail.com>
2020-06-29 16:08:45 +0800

    703 another solution

--- src/main/java/easy/_700_799/_703_kth_largest_ellement_in_a_stream/KthLargest.java
+++ src/main/java/easy/_700_799/_703_kth_largest_ellement_in_a_stream/KthLargest.java
@@ -27,52 +27,115 @@ package easy._700_799._703_kth_largest_ellement_in_a_stream;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class KthLargest {
 
-  List<Integer> arr;
-  int k;
 
   //  解答成功:
   //  执行耗时:23 ms,击败了28.06% 的Java用户
   //  内存消耗:45.3 MB,击败了28.86% 的Java用户
+//  List<Integer> arr;
+//  int k;
+//  public KthLargest(int k, int[] nums) {
+//    this.k = k - 1;
+//    this.arr = new ArrayList<>();
+//    for (int i = 0; i < k; i++) {
+//      arr.add(Integer.MIN_VALUE);
+//    }
+//    for (int i = 0; i < nums.length; i++) {
+//      add(nums[i]);
+//    }
+//  }
+//
+//  public int add(int val) {
+//    if (arr.get(k) < val) {
+//      int l = 0, r = k, mid = (l + r) / 2;
+//      while (l <= r) {
+//        if (val < arr.get(mid)) {
+//          l = mid;
+//        } else if (arr.get(mid) < val) {
+//          r = mid;
+//        } else {
+//          arr.add(mid, val);
+//          arr.remove(k + 1);
+//          break;
+//        }
+//        mid = (l + r) / 2;
+//        if (r - l == 1 || l == r) {
+//          mid = arr.get(l) <= val ? l : r;
+//          arr.add(mid, val);
+//          arr.remove(k + 1);
+//          break;
+//        }
+//      }
+//    }
+//    return arr.get(k);
+//  }
+
+  //  解答成功:
+  //  执行耗时:10 ms,击败了99.95% 的Java用户
+  //  内存消耗:43.4 MB,击败了96.06% 的Java用户
+  int k;
+  int[] heap;
   public KthLargest(int k, int[] nums) {
-    this.k = k - 1;
-    this.arr = new ArrayList<>();
-    for (int i = 0; i < k; i++) {
-      arr.add(Integer.MIN_VALUE);
-    }
-    for (int i = 0; i < nums.length; i++) {
-      add(nums[i]);
+    this.k = k;
+    heap = new int[k];
+    Arrays.fill(heap, Integer.MIN_VALUE);
+    for (int i : nums) {
+      add(i);
     }
   }
 
   public int add(int val) {
-    if (arr.get(k) < val) {
-      int l = 0, r = k, mid = (l + r) / 2;
-      while (l <= r) {
-        if (val < arr.get(mid)) {
-          l = mid;
-        } else if (arr.get(mid) < val) {
-          r = mid;
-        } else {
-          arr.add(mid, val);
-          arr.remove(k + 1);
-          break;
-        }
-        mid = (l + r) / 2;
-        if (r - l == 1 || l == r) {
-          mid = arr.get(l) <= val ? l : r;
-          arr.add(mid, val);
-          arr.remove(k + 1);
-          break;
-        }
-      }
+    if (heap[0] < val) {
+      heap[0] = val;
+      heapSort(0);
     }
-    return arr.get(k);
+    return heap[0];
   }
+
+  public void heapSort(int index) {
+    if (k <= index) {
+      return;
+    }
+    int lChild = left(index), rChild = right(index);
+    if (lChild < k && rChild < k) {
+      if (heap[index] <= heap[lChild] && heap[index] <= heap[rChild]) {
+        return;
+      }
+      if (heap[lChild] <= heap[rChild]) {
+        swap(index, lChild);
+        heapSort(lChild);
+      } else {
+        swap(index, rChild);
+        heapSort(rChild);
+      }
+    } else if (lChild < k && heap[lChild] < heap[index]) {
+      swap(index, lChild);
+      heapSort(lChild);
+    } else if (rChild < k && heap[rChild] < heap[index]) {
+      swap(index, rChild);
+      heapSort(rChild);
+    }
+  }
+
+  public int left(int index) {
+    return 2 * index + 1;
+  }
+
+  public int right(int index) {
+    return 2 * index + 2;
+  }
+
+  public void swap(int i, int j) {
+    heap[i] = heap[i] ^ heap[j];
+    heap[j] = heap[i] ^ heap[j];
+    heap[i] = heap[i] ^ heap[j];
+  }
+
 }
 
 /**
