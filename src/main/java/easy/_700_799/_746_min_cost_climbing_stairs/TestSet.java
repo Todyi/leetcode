919a6249720815374c0653f63957c94257cd2313
Tody <tody.code@gmail.com>
2020-07-27 14:45:21 +0800

    746.Min Cost Climbing Stairs

---
+++ src/main/java/easy/_700_799/_746_min_cost_climbing_stairs/TestSet.java
@@ -0,0 +1,28 @@
+package easy._700_799._746_min_cost_climbing_stairs;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSet {
+
+  @Test
+  public void test0() {
+    int[] cost = {10, 15, 20};
+    Integer result = 15;
+    int solution = new easy._700_799._746_min_cost_climbing_stairs.Solution()
+        .minCostClimbingStairs(cost);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test1() {
+    int[] cost = {1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
+    Integer result = 6;
+    int solution = new easy._700_799._746_min_cost_climbing_stairs.Solution()
+        .minCostClimbingStairs(cost);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+
+}
