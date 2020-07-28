ba73a168e419e65f0f260c7586b5e005b48b522b
Tody <tody.code@gmail.com>
2020-07-28 15:36:37 +0800

    747.Largest Number At Least Twice of Others

---
+++ src/main/java/easy/_700_799/_747_largest_number_at_least_twice_of_others/TestSet.java
@@ -0,0 +1,46 @@
+package easy._700_799._747_largest_number_at_least_twice_of_others;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSet {
+
+  @Test
+  public void test0() {
+    int[] nums = {3, 6, 1, 0};
+    Integer result = 1;
+    int solution = new easy._700_799._747_largest_number_at_least_twice_of_others.Solution()
+        .dominantIndex(nums);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test2() {
+    int[] nums = {1, 2, 3, 4};
+    Integer result = -1;
+    int solution = new easy._700_799._747_largest_number_at_least_twice_of_others.Solution()
+        .dominantIndex(nums);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test3() {
+    int[] nums = {0, 0, 0, 1};
+    Integer result = 3;
+    int solution = new easy._700_799._747_largest_number_at_least_twice_of_others.Solution()
+        .dominantIndex(nums);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test4() {
+    int[] nums = {1, 0};
+    Integer result = 0;
+    int solution = new easy._700_799._747_largest_number_at_least_twice_of_others.Solution()
+        .dominantIndex(nums);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+
+}
