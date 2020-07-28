18c9dffe6467e6aa5e9b66733c7ff1a5cc2834f5
Tody <tody.code@gmail.com>
2020-07-28 16:18:27 +0800

    748.Shortest Completing Word

---
+++ src/main/java/easy/_700_799/_748_shortest_completing_word/TestSet.java
@@ -0,0 +1,40 @@
+package easy._700_799._748_shortest_completing_word;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSet {
+
+  @Test
+  public void test0() {
+    String licensePlate = "1s3 PSt";
+    String[] words = {"step", "steps", "stripe", "stepple"};
+    String result = "steps";
+    String solution = new easy._700_799._748_shortest_completing_word.Solution()
+        .shortestCompletingWord(licensePlate, words);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test1() {
+    String licensePlate = "1s3 456";
+    String[] words = {"looks", "pest", "stew", "show"};
+    String result = "pest";
+    String solution = new easy._700_799._748_shortest_completing_word.Solution()
+        .shortestCompletingWord(licensePlate, words);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test2() {
+    String licensePlate = "Ah71752";
+    String[] words = {"suggest","letter","of","husband","easy","education","drug","prevent","writer","old"};
+    String result = "husband";
+    String solution = new easy._700_799._748_shortest_completing_word.Solution()
+        .shortestCompletingWord(licensePlate, words);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+
+}
