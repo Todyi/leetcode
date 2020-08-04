963b2ae006a35fbd63217221e587db6915616f06
Tody <tody.code@gmail.com>
2020-08-04 14:53:26 +0800

    806.Number of Lines To Write String

---
+++ src/main/java/easy/_800_899/_806_number_of_lines_to_write_string/TestSet.java
@@ -0,0 +1,32 @@
+package easy._800_899._806_number_of_lines_to_write_string;
+
+
+import common.Comparators;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSet {
+
+  @Test
+  public void test0() {
+    int[] widths = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
+        10, 10, 10, 10, 10, 10};
+    String S = "abcdefghijklmnopqrstuvwxyz";
+    int[] result = {3, 60};
+    int[] solution = new easy._800_899._806_number_of_lines_to_write_string.Solution()
+        .numberOfLines(widths, S);
+    Assert.assertTrue(Comparators.isSame(result, solution));
+  }
+
+  @Test
+  public void test1() {
+    int[] widths = {4, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
+        10, 10, 10, 10, 10, 10};
+    String S = "bbbcccdddaaa";
+    int[] result = {2, 4};
+    int[] solution = new easy._800_899._806_number_of_lines_to_write_string.Solution()
+        .numberOfLines(widths, S);
+    Assert.assertTrue(Comparators.isSame(result, solution));
+  }
+
+}
