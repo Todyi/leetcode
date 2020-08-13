b5ad2f9426f4d5e4d463032ab0631676b76a68b6
Tody <tody.code@gmail.com>
2020-08-13 15:00:30 +0800

    849.Maximize Distance To Closest Person

---
+++ src/main/java/easy/_800_899/_849_maximize_distance_to_closest_person/TestSet.java
@@ -0,0 +1,27 @@
+package easy._800_899._849_maximize_distance_to_closest_person;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSet {
+
+  @Test
+  public void test0() {
+    int[] seats = {1, 0, 0, 0, 1, 0, 1};
+    Integer result = 2;
+    int solution = new easy._800_899._849_maximize_distance_to_closest_person.Solution()
+        .maxDistToClosest(seats);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test1() {
+    int[] seats = {1, 0, 0, 0};
+    Integer result = 3;
+    int solution = new easy._800_899._849_maximize_distance_to_closest_person.Solution()
+        .maxDistToClosest(seats);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+}
