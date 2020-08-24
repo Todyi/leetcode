6934c2391a5d9a57751390cda9ec3a23cc9949c1
Tody <tody.code@gmail.com>
2020-08-24 11:22:05 +0800

    908.Smallest Range I

---
+++ src/main/java/easy/_900_999/_908_smallest_range_i/TestSet.java
@@ -0,0 +1,39 @@
+package easy._900_999._908_smallest_range_i;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSet {
+
+  @Test
+  public void test0() {
+    int[] A = {1};
+    int K = 0;
+    Integer result = 0;
+    int solution = new easy._900_999._908_smallest_range_i.Solution()
+        .smallestRangeI(A, K);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test1() {
+    int[] A = {0, 10};
+    int K = 2;
+    Integer result = 6;
+    int solution = new easy._900_999._908_smallest_range_i.Solution()
+        .smallestRangeI(A, K);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test2() {
+    int[] A = {1, 3, 6};
+    int K = 3;
+    Integer result = 0;
+    int solution = new easy._900_999._908_smallest_range_i.Solution()
+        .smallestRangeI(A, K);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+}
