f26789e95593d0e5e68831dc8d08995ba5033351
Tody <tody.code@gmail.com>
2020-08-31 15:27:24 +0800

    942.DI String Match

---
+++ src/main/java/easy/_900_999/_942_di_string_match/TestSet.java
@@ -0,0 +1,38 @@
+package easy._900_999._942_di_string_match;
+
+
+import common.Comparators;
+import common.TreeNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSet {
+
+  @Test
+  public void test0() {
+    String S = "IDID";
+    int[] result = {0, 4, 1, 3, 2};
+    int[] solution = new easy._900_999._942_di_string_match.Solution()
+        .diStringMatch(S);
+    Assert.assertTrue(Comparators.isSame(result, solution));
+  }
+
+  @Test
+  public void test1() {
+    String S = "III";
+    int[] result = {0, 1, 2, 3};
+    int[] solution = new easy._900_999._942_di_string_match.Solution()
+        .diStringMatch(S);
+    Assert.assertTrue(Comparators.isSame(result, solution));
+  }
+
+  @Test
+  public void test2() {
+    String S = "DDI";
+    int[] result = {3, 2, 0, 1};
+    int[] solution = new easy._900_999._942_di_string_match.Solution()
+        .diStringMatch(S);
+    Assert.assertTrue(Comparators.isSame(result, solution));
+  }
+
+}
