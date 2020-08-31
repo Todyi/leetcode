20a33dd86314eaa95f806c57ceb2de67b2e6cacf
Tody <tody.code@gmail.com>
2020-08-31 15:49:32 +0800

    944.Delete Columns to Make Sorted

---
+++ src/main/java/easy/_900_999/_944_delete_columns_to_make_sorted/TestSet.java
@@ -0,0 +1,36 @@
+package easy._900_999._944_delete_columns_to_make_sorted;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSet {
+
+  @Test
+  public void test0() {
+    String[] A = {"cba", "daf", "ghi"};
+    Integer result = 1;
+    int solution = new easy._900_999._944_delete_columns_to_make_sorted.Solution()
+        .minDeletionSize(A);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test1() {
+    String[] A = {"a","b"};
+    Integer result = 0;
+    int solution = new easy._900_999._944_delete_columns_to_make_sorted.Solution()
+        .minDeletionSize(A);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test2() {
+    String[] A = {"zyx","wvu","tsr"};
+    Integer result = 3;
+    int solution = new easy._900_999._944_delete_columns_to_make_sorted.Solution()
+        .minDeletionSize(A);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+}
