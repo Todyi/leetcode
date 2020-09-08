ace0ce6d441dcd90aae144a23d0cb18d456336f3
Tody <tody.code@gmail.com>
2020-09-08 15:23:11 +0800

    1025 another solution

--- src/main/java/easy/_1000_1099/_1025_divisor_game/Solution.java
+++ src/main/java/easy/_1000_1099/_1025_divisor_game/Solution.java
@@ -55,20 +55,40 @@ class Solution {
   //  解答成功:
   //  执行耗时:2 ms,击败了29.09% 的Java用户
   //  内存消耗:35.9 MB,击败了93.06% 的Java用户
+//  public boolean divisorGame(int N) {
+//    boolean[] map = new boolean[N + 1];
+//    if (1 < N) {
+//      map[2] = true;
+//    }
+//    for (int i = 4; i <= N; i++) {
+//      for (int j = i - 1; 2 * j > i && j > 1; j--) {
+//        if (!map[j] && i % (i - j) == 0) {
+//          map[i] = true;
+//          break;
+//        }
+//      }
+//    }
+//    return map[N];
+//  }
+
+  //  解答成功:
+  //  执行耗时:0 ms,击败了100.00% 的Java用户
+  //  内存消耗:35.7 MB,击败了98.78% 的Java用户
   public boolean divisorGame(int N) {
-    boolean[] map = new boolean[N + 1];
-    if (1 < N) {
-      map[2] = true;
-    }
-    for (int i = 4; i <= N; i++) {
-      for (int j = i - 1; 2 * j > i && j > 1; j--) {
-        if (!map[j] && i % (i - j) == 0) {
-          map[i] = true;
-          break;
-        }
-      }
-    }
-    return map[N];
+    //even = 1 + odd
+    //odd +- odd = even
+    //odd can only be divisible by another odd
+    //→odd can only change to even,even can change to both odd and even
+
+    //2:true
+    //3:false
+    //4:true
+    //5:false
+    //6:true
+    //7:false
+    //...
+    //N:true(N%2==0)
+    return (N & 1) == 0;
   }
 }
 //leetcode submit region end(Prohibit modification and deletion)
