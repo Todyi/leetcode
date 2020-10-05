0d3c26bf306bbb47ef46fee99362fb5c5730872c
Tody <tody.code@gmail.com>
2020-10-05 15:58:28 +0800

    1408 another solution

--- src/main/java/easy/_1400_1499/_1408_string_matching_in_an_array/Solution.java
+++ src/main/java/easy/_1400_1499/_1408_string_matching_in_an_array/Solution.java
@@ -51,72 +51,91 @@ class Solution {
 
   //  Runtime: 3 ms, faster than 89.77% of Java online submissions for String Matching in an Array.
   //  Memory Usage: 39.8 MB, less than 38.25% of Java online submissions for String Matching in an Array.
-  class Node {
+//  class Node {
+//
+//    String word;
+//    Node[] sub;
+//    boolean picked = false;
+//
+//    public Node() {
+//    }
+//  }
+//
+//  public void addSub(int i, char[] arr, Node[] map, String word) {
+//    if (i < arr.length) {
+//      int idx = arr[i] - 'a';
+//      if (map[idx] == null) {
+//        map[idx] = new Node();
+//      }
+//      Node point = map[idx];
+//      if (i == arr.length - 1 && point.word == null) {
+//        point.word = word;
+//        return;
+//      }
+//      if (point.sub == null && i < arr.length - 1) {
+//        point.sub = new Node[26];
+//      }
+//      addSub(i + 1, arr, point.sub, word);
+//    }
+//  }
+//
+//  public void checkSub(int i, char[] arr, Node[] map, String word) {
+//    if (i < arr.length) {
+//      if (map == null) {
+//        return;
+//      }
+//      int idx = arr[i] - 'a';
+//      Node point = map[idx];
+//      if (point == null) {
+//        return;
+//      }
+//      if (point.word != null && !point.picked) {
+//        res.add(point.word);
+//        point.picked = true;
+//      }
+//      if (i == arr.length - 1 && !point.picked && word != null) {
+//        res.add(word);
+//        point.picked = true;
+//        return;
+//      }
+//      checkSub(i + 1, arr, point.sub, word);
+//    }
+//  }
+//
+//  Node[] map = new Node[26];
+//  List<String> res = new ArrayList<>();
+//
+//  public List<String> stringMatching(String[] words) {
+//    for (String word : words) {
+//      int idx = word.charAt(0) - 'a';
+//      if (map[idx] == null) {
+//        map[idx] = new Node();
+//      }
+//      char[] arr = word.toCharArray();
+//      checkSub(0, arr, map, word);
+//      addSub(0, arr, map, word);
+//      for (int i = 1; i < arr.length; i++) {
+//        checkSub(i, arr, map, null);
+//        addSub(i, arr, map, null);
+//      }
+//    }
+//    return res;
+//  }
 
-    String word;
-    Node[] sub;
-    boolean picked = false;
-
-    public Node() {
-    }
-  }
-
-  public void addSub(int i, char[] arr, Node[] map, String word) {
-    if (i < arr.length) {
-      int idx = arr[i] - 'a';
-      if (map[idx] == null) {
-        map[idx] = new Node();
-      }
-      Node point = map[idx];
-      if (i == arr.length - 1 && point.word == null) {
-        point.word = word;
-        return;
-      }
-      if (point.sub == null && i < arr.length - 1) {
-        point.sub = new Node[26];
-      }
-      addSub(i + 1, arr, point.sub, word);
-    }
-  }
-
-  public void checkSub(int i, char[] arr, Node[] map, String word) {
-    if (i < arr.length) {
-      if (map == null) {
-        return;
-      }
-      int idx = arr[i] - 'a';
-      Node point = map[idx];
-      if (point == null) {
-        return;
-      }
-      if (point.word != null && !point.picked) {
-        res.add(point.word);
-        point.picked = true;
-      }
-      if (i == arr.length - 1 && !point.picked && word != null) {
-        res.add(word);
-        point.picked = true;
-        return;
-      }
-      checkSub(i + 1, arr, point.sub, word);
-    }
-  }
-
-  Node[] map = new Node[26];
-  List<String> res = new ArrayList<>();
-
+  //  解答成功:
+  //  执行耗时:1 ms,击败了100.00% 的Java用户
+  //  内存消耗:38.9 MB,击败了99.15% 的Java用户
   public List<String> stringMatching(String[] words) {
+    List<String> res = new ArrayList<>();
+    StringBuilder sb = new StringBuilder();
     for (String word : words) {
-      int idx = word.charAt(0) - 'a';
-      if (map[idx] == null) {
-        map[idx] = new Node();
-      }
-      char[] arr = word.toCharArray();
-      checkSub(0, arr, map, word);
-      addSub(0, arr, map, word);
-      for (int i = 1; i < arr.length; i++) {
-        checkSub(i, arr, map, null);
-        addSub(i, arr, map, null);
+      sb.append(word).append("|");
+    }
+    for (String word : words) {
+      int idx0 = sb.indexOf(word, 0);
+      int idx1 = sb.indexOf(word, idx0 + word.length());
+      if (idx1 != -1){
+        res.add(word);
       }
     }
     return res;
