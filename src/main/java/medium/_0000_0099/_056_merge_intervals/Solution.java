7501c71ca1dfe2b62075ba01848ba6150754c458
Tody <tody.code@gmail.com>
2020-11-04 14:51:33 +0800

    56 another solution

--- src/main/java/medium/_0000_0099/_056_merge_intervals/Solution.java
+++ src/main/java/medium/_0000_0099/_056_merge_intervals/Solution.java
@@ -37,51 +37,80 @@ class Solution {
 
   //  Runtime: 2 ms, faster than 99.60% of Java online submissions for Merge Intervals.
   //  Memory Usage: 41.5 MB, less than 10.80% of Java online submissions for Merge Intervals.
+//  public int[][] merge(int[][] intervals) {
+//    int idx = 0, cur = 0, len = intervals.length;
+//    if (len < 2) {
+//      return intervals;
+//    }
+//    quickSort(intervals, 0, len - 1);
+//    while (cur < len) {
+//      if (intervals[idx][1] < intervals[cur][0]) {
+//        idx++;
+//        intervals[idx][0] = intervals[cur][0];
+//        intervals[idx][1] = intervals[cur][1];
+//      } else {
+//        if (intervals[idx][0] <= intervals[cur][0] && intervals[cur][0] <= intervals[idx][1]) {
+//          intervals[idx][1] = Math.max(intervals[idx][1], intervals[cur][1]);
+//        }
+//      }
+//      cur++;
+//    }
+//    int[][] res = new int[++idx][2];
+//
+//    for (int i = 0; i < idx; i++) {
+//      res[i] = intervals[i];
+//    }
+//    return res;
+//  }
+//
+//  public void quickSort(int[][] intervals, int start, int end) {
+//    if (end <= start) {
+//      return;
+//    }
+//    int pivot = intervals[end][0], cur = start - 1, idx = start;
+//    while (++cur < end) {
+//      if (intervals[cur][0] < pivot) {
+//        swap(intervals, cur, idx++);
+//      }
+//    }
+//    swap(intervals, end, idx);
+//    quickSort(intervals, start, idx - 1);
+//    quickSort(intervals, idx + 1, end);
+//  }
+//
+//  public void swap(int[][] intervals, int i, int j) {
+//    int[] tmp = intervals[i];
+//    intervals[i] = intervals[j];
+//    intervals[j] = tmp;
+//  }
+
+  //  解答成功:
+  //  执行耗时:0 ms,击败了100.00% 的Java用户
+  //  内存消耗:41.6 MB,击败了10.80% 的Java用户
   public int[][] merge(int[][] intervals) {
-    int idx = 0, cur = 0, len = intervals.length;
-    if (len < 2) {
-      return intervals;
-    }
-    quickSort(intervals, 0, len - 1);
-    while (cur < len) {
-      if (intervals[idx][1] < intervals[cur][0]) {
-        idx++;
-        intervals[idx][0] = intervals[cur][0];
-        intervals[idx][1] = intervals[cur][1];
-      } else {
-        if (intervals[idx][0] <= intervals[cur][0] && intervals[cur][0] <= intervals[idx][1]) {
-          intervals[idx][1] = Math.max(intervals[idx][1], intervals[cur][1]);
+    int len = intervals.length, count = len, idx = 0;
+    for (int i = 0; i < len - 1; i++) {
+      int[] s1 = intervals[i];
+      for (int j = i + 1; j < len; j++) {
+        int[] s2 = intervals[j];
+        if (s1[0] <= s2[1] && s2[0] <= s1[1]) {
+          s2[0] = Math.min(s1[0], s2[0]);
+          s2[1] = Math.max(s1[1], s2[1]);
+          s1[0] = 1;
+          s1[1] = 0;
+          count--;
+          break;
         }
       }
-      cur++;
     }
-    int[][] res = new int[++idx][2];
-
-    for (int i = 0; i < idx; i++) {
-      res[i] = intervals[i];
+    int[][] res = new int[count][];
+    for (int i = 0; i < len; i++) {
+      if (intervals[i][1] < intervals[i][0]) {
+        continue;
+      }
+      res[idx++] = intervals[i];
     }
     return res;
-  }
-
-  public void quickSort(int[][] intervals, int start, int end) {
-    if (end <= start) {
-      return;
-    }
-    int pivot = intervals[end][0], cur = start - 1, idx = start;
-    while (++cur < end) {
-      if (intervals[cur][0] < pivot) {
-        swap(intervals, cur, idx++);
-      }
-    }
-    swap(intervals, end, idx);
-    quickSort(intervals, start, idx - 1);
-    quickSort(intervals, idx + 1, end);
-  }
-
-  public void swap(int[][] intervals, int i, int j) {
-    int[] tmp = intervals[i];
-    intervals[i] = intervals[j];
-    intervals[j] = tmp;
   }
 }
 //leetcode submit region end(Prohibit modification and deletion)
