92892d0dc77c35ca0f96d70459c78a955fb61719
Tody <tody.code@gmail.com>
2021-02-02 16:04:58 +0800

    178.Rank Scores

---
+++ src/main/java/medium/_0100_0199/_178_rank_scores/[178]Rank Scores.sql
@@ -0,0 +1,45 @@
+--Write a SQL query to rank scores. If there is a tie between two scores, 
+-- both should have the same ranking. Note that after a tie, 
+-- the next ranking number should be the next consecutive integer value. 
+-- In other words, there should be no "holes" between ranks. 
+--
+-- 
+--+----+-------+
+--| Id | Score |
+--+----+-------+
+--| 1  | 3.50  |
+--| 2  | 3.65  |
+--| 3  | 4.00  |
+--| 4  | 3.85  |
+--| 5  | 4.00  |
+--| 6  | 3.65  |
+--+----+-------+
+-- 
+--
+-- For example, given the above Scores table, your query should generate the fol
+--lowing report (order by highest score): 
+--
+-- 
+--+-------+---------+
+--| score | Rank    |
+--+-------+---------+
+--| 4.00  | 1       |
+--| 4.00  | 1       |
+--| 3.85  | 2       |
+--| 3.65  | 3       |
+--| 3.65  | 3       |
+--| 3.50  | 4       |
+--+-------+---------+
+-- 
+--
+-- Important Note: For MySQL solutions, to escape reserved words used as column 
+--names, you can use an apostrophe before and after the keyword. For example `Rank
+--`. 
+-- 👍 862 👎 160
+
+
+--There is no code of Java type for this problem
+
+-- Runtime: 212 ms, faster than 97.28% of MySQL online submissions for Rank Scores.
+-- Memory Usage: 0B, less than 100.00% of MySQL online submissions for Rank Scores.
+SELECT Score,DENSE_RANK() OVER (ORDER BY Score DESC) AS `Rank` FROM Scores;
