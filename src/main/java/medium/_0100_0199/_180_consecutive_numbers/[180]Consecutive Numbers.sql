e5ff2d0fec40aef770038b010e27475590324ae6
Tody <tody.code@gmail.com>
2021-02-03 16:50:48 +0800

    180.Consecutive Numbers

---
+++ src/main/java/medium/_0100_0199/_180_consecutive_numbers/[180]Consecutive Numbers.sql
@@ -0,0 +1,59 @@
+--Table: Logs 
+--
+-- 
+--+-------------+---------+
+--| Column Name | Type    |
+--+-------------+---------+
+--| id          | int     |
+--| num         | varchar |
+--+-------------+---------+
+--id is the primary key for this table.
+-- 
+--
+-- 
+--
+-- Write an SQL query to find all numbers that appear at least three times consecutively.
+--
+-- Return the result table in any order. 
+--
+-- The query result format is in the following example: 
+--
+-- 
+--
+-- 
+--Logs table:
+--+----+-----+
+--| Id | Num |
+--+----+-----+
+--| 1  | 1   |
+--| 2  | 1   |
+--| 3  | 1   |
+--| 4  | 2   |
+--| 5  | 1   |
+--| 6  | 2   |
+--| 7  | 2   |
+--+----+-----+
+--
+--Result table:
+--+-----------------+
+--| ConsecutiveNums |
+--+-----------------+
+--| 1               |
+--+-----------------+
+--1 is the only number that appears consecutively for at least three times.
+-- 
+-- 👍 550 👎 127
+
+
+--There is no code of Java type for this problem
+
+-- Runtime: 360 ms, faster than 97.86% of MySQL online submissions for Consecutive Numbers.
+-- Memory Usage: 0B, less than 100.00% of MySQL online submissions for Consecutive Numbers.
+SELECT DISTINCT Num as ConsecutiveNums
+FROM(
+   SELECT
+       lead(Num) OVER () AS leadNum,
+       Num,
+       lag(Num) OVER () AS lagNum
+   FROM Logs
+) AS t WHERE t.Num = t.leadNum AND t.Num = t.lagNum;
