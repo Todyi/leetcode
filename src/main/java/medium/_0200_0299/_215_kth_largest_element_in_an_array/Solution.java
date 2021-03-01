e498ee380448403159120ec33096d5adc252d0c5
Tody <tody.code@gmail.com>
2021-03-01 16:24:06 +0800

    215.Kth Largest Element in an Array(Quick Sort)

---
+++ src/main/java/medium/_0200_0299/_215_kth_largest_element_in_an_array/Solution.java
@@ -0,0 +1,63 @@
+package medium._0200_0299._215_kth_largest_element_in_an_array;
+
+//Find the kth largest element in an unsorted array. Note that it is the kth lar
+//gest element in the sorted order, not the kth distinct element.
+//
+// Example 1:
+//
+//
+//Input: [3,2,1,5,6,4] and k = 2
+//Output: 5
+//
+//
+// Example 2:
+//
+//
+//Input: [3,2,3,1,2,4,5,5,6] and k = 4
+//Output: 4
+//
+// Note:
+//You may assume k is always valid, 1 ≤ k ≤ array's length.
+// Related Topics Divide and Conquer Heap
+// 👍 5206 👎 336
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+  //  Runtime: 16 ms, faster than 14.88% of Java online submissions for Kth Largest Element in an Array.
+  //  Memory Usage: 39.6 MB, less than 30.94% of Java online submissions for Kth Largest Element in an Array.
+  public int findKthLargest(int[] nums, int k) {
+    return quickSort(nums, 0, nums.length - 1, k);
+  }
+
+  public int quickSort(int[] nums, int l, int r, int k) {
+    if (r <= l) {
+      return nums[l];
+    }
+    int pivot = nums[r], idx = l;
+    for (int i = l; i < r; i++) {
+      if (pivot < nums[i]) {
+        swap(nums, i, idx++);
+      }
+    }
+    if (nums[idx] < pivot) {
+      swap(nums, r, idx);
+    }
+    if (k - 1 == idx) {
+      return nums[idx];
+    } else if (k - 1 < idx) {
+      return quickSort(nums, l, idx - 1, k);
+    } else {
+      return quickSort(nums, idx + 1, r, k);
+    }
+  }
+
+  public void swap(int[] nums, int i, int j) {
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+  }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
---
+++ src/main/java/medium/_0200_0299/_215_kth_largest_element_in_an_array/TestSet.java
@@ -0,0 +1,39 @@
+package medium._0200_0299._215_kth_largest_element_in_an_array;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSet {
+
+  @Test
+  public void test0() {
+    int[] nums = {3, 2, 1, 5, 6, 4};
+    int k = 2;
+    Integer result = 5;
+    int solution = new medium._0200_0299._215_kth_largest_element_in_an_array.Solution()
+        .findKthLargest(nums, k);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test1() {
+    int[] nums = {3, 2, 3, 1, 2, 4, 5, 5, 6};
+    int k = 4;
+    Integer result = 4;
+    int solution = new medium._0200_0299._215_kth_largest_element_in_an_array.Solution()
+        .findKthLargest(nums, k);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+  @Test
+  public void test2() {
+    int[] nums = {-1, 2, 0};
+    int k = 2;
+    Integer result = 0;
+    int solution = new medium._0200_0299._215_kth_largest_element_in_an_array.Solution()
+        .findKthLargest(nums, k);
+    Assert.assertTrue(result.equals(solution));
+  }
+
+}
