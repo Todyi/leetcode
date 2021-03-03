269728bff7a66e3c63e6d32a44d60611cab46c58
Tody <tody.code@gmail.com>
2021-03-03 14:19:54 +0800

    221 another solution

--- src/main/java/medium/_0200_0299/_221_maximal_square/Solution.java
+++ src/main/java/medium/_0200_0299/_221_maximal_square/Solution.java
@@ -45,29 +45,70 @@ class Solution {
   //  解答成功:
   //  执行耗时:3 ms,击败了98.60% 的Java用户
   //  内存消耗:41.4 MB,击败了98.88% 的Java用户
+//  public int maximalSquare(char[][] matrix) {
+//    int m = matrix.length, n = matrix[0].length, max = 0;
+//    int[][] map = new int[m][n];
+//    for (int i = 0; i < m; i++) {
+//      for (int j = 0; j < n; j++) {
+//        if (matrix[i][j] == '1') {
+//          if (0 < i && 0 < j && matrix[i - 1][j - 1] == '1') {
+//            int temp = map[i - 1][j - 1], curMax = temp;
+//            for (; temp > 0; temp--) {
+//              if (i - temp < 0 || matrix[i - temp][j] == '0'
+//                  || j - temp < 0 || matrix[i][j - temp] == '0') {
+//                curMax = temp - 1;
+//              }
+//            }
+//            map[i][j] = curMax + 1;
+//          } else {
+//            map[i][j] = 1;
+//          }
+//          max = Math.max(map[i][j], max);
+//        }
+//      }
+//    }
+//    return max * max;
+//  }
+
+
+  //  解答成功:
+  //  执行耗时:1 ms,击败了100.00% 的Java用户
+  //  内存消耗:42.4 MB,击败了40.65% 的Java用户
+  int m, n;
+
   public int maximalSquare(char[][] matrix) {
-    int m = matrix.length, n = matrix[0].length, max = 0;
-    int[][] map = new int[m][n];
-    for (int i = 0; i < m; i++) {
+    m = matrix.length;
+    n = matrix[0].length;
+    return max_dfs(matrix, 1, 0);
+  }
+
+  public int max_dfs(char[][] matrix, int maxLen, int curRow) {
+    for (int i = curRow; i < m; i++) {
       for (int j = 0; j < n; j++) {
-        if (matrix[i][j] == '1') {
-          if (0 < i && 0 < j && matrix[i - 1][j - 1] == '1') {
-            int temp = map[i - 1][j - 1], curMax = temp;
-            for (; temp > 0; temp--) {
-              if (i - temp < 0 || matrix[i - temp][j] == '0'
-                  || j - temp < 0 || matrix[i][j - temp] == '0') {
-                curMax = temp - 1;
-              }
-            }
-            map[i][j] = curMax + 1;
-          } else {
-            map[i][j] = 1;
-          }
-          max = Math.max(map[i][j], max);
+        if (isSquare(matrix, maxLen, i, j)) {
+          return Math.max(maxLen * maxLen, max_dfs(matrix, maxLen + 1, i));
         }
       }
     }
-    return max * max;
+    return 0;
+  }
+
+  public boolean isSquare(char[][] matrix, int maxLen, int i, int j) {
+    if (maxLen == 1 && matrix[i][j] == '1') {
+      return true;
+    }
+    int maxI = i + maxLen, maxJ = j + maxLen;
+    if (m < maxI || n < maxJ) {
+      return false;
+    }
+    for (int r = i; r < maxI; r++) {
+      for (int c = j; c < maxJ; c++) {
+        if (matrix[r][c] == '0') {
+          return false;
+        }
+      }
+    }
+    return true;
   }
 }
 //leetcode submit region end(Prohibit modification and deletion)
